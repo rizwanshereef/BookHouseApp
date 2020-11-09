5cba1d85bda457381f588cd8de31d58d7bcb7368
Ubuntu <subhramkumar10@gmail.com>
2020-11-09 05:40:02 +0000

    Added register components

--- BookUI/src/app/app.module.ts
+++ BookUI/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
