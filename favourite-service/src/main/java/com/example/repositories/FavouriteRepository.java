117cd4bcec8554b53c114747dd13ce1f9668cc1e
rsrizwan77 <rsrizwan77@gmail.com>
2020-11-10 22:55:38 +0530

    updated favorite-repo

--- favourite-service/src/main/java/com/example/repositories/FavouriteRepository.java
+++ favourite-service/src/main/java/com/example/repositories/FavouriteRepository.java
@@ -3,16 +3,13 @@ package com.example.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
 import com.example.model.Favourite;
 
-@Repository
 public interface FavouriteRepository extends MongoRepository<Favourite,String> {
 	
-	public List<Favourite> findByUsername(String name);
-	public Favourite deleteByBookIdandUsername(String id, String username);
-	public List<Favourite> findByBookIdAndUsername(String id, String username);
+	List<Favourite> findByUserId(String userId);
+
 	
 
 }
