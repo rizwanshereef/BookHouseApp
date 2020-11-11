dcb71d914a8f38dec54908734fc70867396826ca
saisravs416 <saisravs416@gmail.com>
2020-11-11 13:03:39 +0000

    added book module

---
+++ BookUI/src/app/modules/books/book.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatDialogModule } from '@angular/material/dialog';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {  MatIconModule } from '@angular/material/icon'
+import { TokenInterceptor } from 'src/app/modules/books/interceptor.service';
+import { FavouriteService } from './favourite.service';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FavouriteComponent } from './favourite/favourite.component';
+import { RecommendationComponent } from './recommendation/recommendation.component';
+
+@NgModule({
+  declarations: [DashboardComponent,FavouriteComponent, RecommendationComponent],
+  imports: [
+    CommonModule, HttpClientModule, AppRoutingModule, MatCardModule,
+    MatButtonModule,MatSnackBarModule,MatDialogModule, 
+    MatDialogModule, MatInputModule,FormsModule,
+    ReactiveFormsModule, MatFormFieldModule,MatIconModule
+  ],
+  // entryComponents:[MovieDialogComponent],
+  exports:[AppRoutingModule, DashboardComponent,FavouriteComponent 
+    
+  ],
+  providers:[FavouriteService,],
+})
+export class BookModule { }
---
+++ BookUI/src/app/modules/books/book.ts
@@ -0,0 +1,6 @@
+export class book{
+    id: Number;
+    title: String;
+    author_name: String;
+    isbn: String;
+}
