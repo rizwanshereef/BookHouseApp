de6302b1500d4262c17c2db8cf6a21fa879a79e4
Ubuntu <subhramkumar10@gmail.com>
2020-11-11 12:52:42 +0000

    Updating components

--- BookUI/src/app/app-routing.module.ts
+++ BookUI/src/app/app-routing.module.ts
@@ -1,10 +1,48 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthguardService } from './authguard.service';
+import { DashboardComponent } from './modules/books/dashboard/dashboard.component';
+import { FavouriteComponent } from './modules/books/favourite/favourite.component';
+import { SearchComponent } from './modules/books/search/search.component';
+import { RecommendationComponent } from './modules/books/recommendation/recommendation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    children:[
+      {
+        path: '',
+        redirectTo: '/login',
+        pathMatch: 'full',
+        canActivate:[AuthguardService]
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+        canActivate : [AuthguardService]
+      },
+      {
+        path: 'favourite',
+        component: FavouriteComponent,
+        canActivate : [AuthguardService]
+      },
+      {
+        path: 'recommendation',
+        component: RecommendationComponent,
+        canActivate : [AuthguardService]
+      },
+      {
+        path: 'search',
+        component: SearchComponent,
+        canActivate : [AuthguardService]
+      },
 
+    ]
+  }];
+  
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
