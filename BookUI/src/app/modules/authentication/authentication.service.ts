d5cf7e073750632a3d61a7cc54d8e88d11ceee97
saisravs416 <saisravs416@gmail.com>
2020-11-11 12:09:35 +0000

    adding authentication service

---
+++ BookUI/src/app/modules/authentication/authentication.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { decode } from "@angular/router/src/url_tree";
+export const TOKEN_NAME:string='jwt_token';
+import * as jwt_decode from 'jwt-decode';
+import { user } from "./User";
+
+
+@Injectable()
+export class AuthenticationService{
+    springEndPoint: string;
+    token:string;
+    user: any;
+    constructor(private httpClient: HttpClient){
+        this.springEndPoint="http://localhost:8080/api/userservice";       
+    }
+
+    registerUser(newUser){
+        const url=this.springEndPoint+"/register";
+        console.log(newUser);
+        return this.httpClient.post(url,newUser, {responseType: 'text'});
+    }
+
+    loginUser(newUser){
+        const url=this.springEndPoint+"/login";
+        console.log('newUser', newUser);
+        return this.httpClient.post(url,newUser);
+    }
+    
+    setToken(token:string){
+        return localStorage.setItem(TOKEN_NAME,token);
+    }
+
+    getToken(){
+        return localStorage.getItem(TOKEN_NAME);
+    }
+
+    deleteToken(){
+        return localStorage.removeItem(TOKEN_NAME);
+    }
+
+    getTokenExpirationDate(token: string): Date{
+        const decoded=jwt_decode(token);
+        if (decoded.exp === undefined) return null;
+        const date=new Date(0);
+        date.setUTCSeconds(decoded.exp);
+        return date;
+    }
+
+    isTokenExpired(token?: string): boolean{
+        if(!token) token=this.getToken();
+        if(!token) return true;
+        const date =this.getTokenExpirationDate(token);
+        if (date===undefined || date===null) return false;
+        return !(date.valueOf() > new Date().valueOf());
+    }
+    }
