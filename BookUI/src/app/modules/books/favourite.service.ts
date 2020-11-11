da66ee22a1080aa55b00f02f5cdb3bf8d1ec32bc
Ubuntu <subhramkumar10@gmail.com>
2020-11-11 12:38:46 +0000

    Adding favouriteservices.ts

---
+++ BookUI/src/app/modules/books/favourite.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { book } from './book';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+};
+
+@Injectable()
+export class FavouriteService{
+  saveUrl: string = "http://localhost:9898/api/bookservice/book";
+  getUrl: string = "http://localhost:9898/api/bookservice/books/";
+delUrl: string="http://localhost:9898/api/bookservice/book/";
+    constructor(private http: HttpClient) { }
+    
+    getBooks(apiUrl): Observable<any> {
+      return this.http.get<any>(apiUrl);
+    }
+
+
+    saveBook(saveBook){
+      console.log(saveBook);
+      return this.http.post(this.saveUrl, saveBook);
+    }
+
+    
+    getFavourites(userId): Observable<any> {
+      return this.http.get<any>(this.getUrl);
+    }
+
+    getMyFavourites(): Observable<Array<book>>{
+      return this.http.get<Array<book>>("http://localhost:9898/api/bookservice/books");
+  }
+
+    deleteFromFavouriteList(book:book){
+      console.log(book);
+      
+      return this.http.delete(this.delUrl+book.id);
+  }
+    
+}
---
+++ BookUI/src/app/modules/books/interceptor.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthenticationService } from '../authentication/authentication.service';
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor{
+
+  constructor(private auth:AuthenticationService) { }
+
+  intercept(request:HttpRequest<any>,next:HttpHandler):Observable<HttpEvent<any>>{
+    console.log(this.auth.getToken());
+    request=request.clone({
+      setHeaders:{
+        Authorization: `Bearer ${this.auth.getToken()}`
+      }
+    });
+    return next.handle(request);
+  }
+}
+
