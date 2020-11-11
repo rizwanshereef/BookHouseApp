fffacc082b5cbed37f0243b3ded5f02aa287efa2
rsrizwan77 <rsrizwan77@gmail.com>
2020-11-11 17:58:09 +0530

    Adding fav-service exception

---
+++ favourite-service/src/main/java/com/stackroute/favouriteservice/exception/FavouriteNotFoundException.java
@@ -0,0 +1,12 @@
+package com.stackroute.favouriteservice.exception;
+
+@SuppressWarnings("serial")
+public class FavouriteNotFoundException extends Exception {
+
+	public FavouriteNotFoundException(String message) {
+		super(message);
+	}
+
+	
+
+}
