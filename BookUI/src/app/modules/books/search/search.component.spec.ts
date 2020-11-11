bf65bc7939440c8d1ac70e63ff0e1efc022ad689
saisravs416 <saisravs416@gmail.com>
2020-11-11 12:44:24 +0000

    pushing search component

---
+++ BookUI/src/app/modules/books/search/search.component.spec.ts
@@ -0,0 +1,93 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FavouriteService } from '../favourite.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { Routes } from '@angular/router';
+import { FavouriteComponent } from '../favourite/favourite.component';
+import { of } from 'rxjs';
+import { LoginComponent } from '../../authentication/login/login.component';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { RegisterComponent } from '../../authentication/register/register.component';
+import { TokenInterceptor } from '../interceptor.service';
+import { AuthenticationService } from '../../authentication/authentication.service';
+import { SearchComponent } from './search.component';
+import { DashboardComponent } from '../dashboard/dashboard.component';
+import { MatLabel, MatCardModule, MatButtonModule, MatSnackBarModule, MatDialogModule, MatInputModule, MatFormFieldModule, MatIconModule, MatSelectModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+fdescribe('SearchComponent', () => {
+  let component: SearchComponent;
+  let fixture: ComponentFixture<SearchComponent>;
+  let homeservice: FavouriteService;
+
+  beforeEach(async(() => {
+
+    
+    TestBed.configureTestingModule({
+      declarations: [ SearchComponent, DashboardComponent,
+      LoginComponent, RegisterComponent,
+    FavouriteComponent ],
+      imports : [
+        BrowserModule, BrowserAnimationsModule,
+        AppRoutingModule,
+        ReactiveFormsModule,
+        HttpClientModule, MatSelectModule,
+        FormsModule,
+        MatCardModule,
+        MatButtonModule,MatSnackBarModule,MatDialogModule, 
+        MatDialogModule, MatInputModule,FormsModule,
+        ReactiveFormsModule, MatFormFieldModule,MatIconModule
+      ],
+      providers : [
+        FavouriteService,
+        { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },
+        AuthenticationService
+      ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SearchComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+  
+  it('able to show the searched book details', () => {
+    let searchKey : string = 'harry';
+    homeservice = fixture.debugElement.injector.get(FavouriteService);
+    const bookapi: any = JSON.parse(JSON.stringify({
+      json : {
+        datafound : 121,
+        datadocs : [{
+          isbn : '3543'
+        },
+      ]
+      }
+    }));
+    spyOn(homeservice, "getBooks").and.returnValue(of(bookapi));
+    component.search(searchKey);
+    expect(homeservice.getBooks).toHaveBeenCalled();
+  });
+
+  it('invalid search! no such book', () => {
+    let searchKey : string = 'harry';
+    homeservice = fixture.debugElement.injector.get(FavouriteService);
+    const bookapi: any = JSON.parse(JSON.stringify({
+      json : {
+      }
+    }));
+    spyOn(homeservice, "getBooks").and.returnValue(of(bookapi));
+    component.search(searchKey);
+    expect(homeservice.getBooks).toHaveBeenCalled();
+    expect(component.errorMessage).toBe('No such result found');
+  });
+
+});
---
+++ BookUI/src/app/modules/books/search/search.component.ts
@@ -0,0 +1,128 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { book } from '../book';
+import { FavouriteService } from '../favourite.service';
+import { AuthenticationService } from '../../authentication/authentication.service';
+import { MatSnackBar } from '@angular/material';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+  beforeSearch: boolean = true;
+  afterSearch: boolean = false;
+  staticBookList: any;
+  bks: any;
+  searchText: any;
+  apiUrl: string;
+  bookList: any = [];
+  errorDiv: boolean = false;
+  errorMessage: any;
+  SearchForm: FormGroup;
+  selected: any;
+  books: Array<book>;
+  dbBookList: any = [];
+  modifiedList = [];
+  book: book;
+  isFavourite: boolean;
+  selectedCategory: any;
+  categories = ['author', 'title', 'any']
+  
+  constructor(private snackbar: MatSnackBar,private fb: FormBuilder,private favouriteservice: FavouriteService,private authSerice: AuthenticationService) { }
+
+  ngOnInit() {
+    this.favouriteservice.getFavourites(this.authSerice.user).subscribe(
+      data => {
+        console.log(data);
+        if (data.length > 0) {
+          this.dbBookList = data;
+        }
+      }
+    );
+  }
+
+
+  search(searchKey) {
+    console.log(searchKey);
+    console.log(this.selected);
+    this.afterSearch = true;
+    this.beforeSearch = false;
+    this.errorDiv = false;
+    this.bookList = new Array<any>();
+    if (searchKey == 'any') {
+      this.selected = 'q';
+    }
+    this.checkAlready();
+    this.apiUrl = 'http://openlibrary.org/search.json?' + this.selected + '=' + searchKey;
+    this.favouriteservice.getBooks(this.apiUrl).subscribe(
+      data => {
+        console.log(data);
+        if (data.numFound > 0) {
+          this.bks = this.formatObjects(data.docs);
+          for (let book of this.bks) {
+            if ((book.isbn != null && book.isbn.length > 1) && book.author_name != undefined) {
+              this.bookList.push(book);
+            }
+          }
+          this.checkAlready();
+        } else {
+          this.errorDiv = true;
+          this.errorMessage = 'No such result found';
+        }
+      },
+      error => {
+        console.log(error);
+        this.errorDiv = true;
+        this.errorMessage = 'Retry';
+      }
+    );
+  }
+
+  checkAlready() {
+    if (this.bookList.length > 0) {
+      for (let book of this.bookList) {
+        for (let bk of this.dbBookList) {
+          if (book.isbn[0] === bk.isbn) {
+            book.isFavourite = false;
+          }
+        }
+      }
+    }
+  }
+
+  formatObjects(objects) {
+    let books = [];
+    for (let obj of objects) {
+      let modified = {
+        title: obj.title,
+        author_name: obj.author_name,
+        isbn: obj.isbn,
+        isFavourite: true,
+        key: obj.key,
+      };
+      books.push(modified);
+    }
+    return books;
+  }
+
+
+  favourite(book){
+    let message=`${book.title} add to favourites`;
+    console.log(book);
+    console.log(this.authSerice.user);
+      let saveBook = {
+      title: book.title,
+      author_name: book.author_name[0],
+      isbn: book.isbn[0],
+    };
+    this.favouriteservice.saveBook(saveBook).subscribe(book=>{
+      console.log("book saved");
+      this.snackbar.open(message, '', {
+        duration:1000
+      });
+    })
+  }
+
+}
