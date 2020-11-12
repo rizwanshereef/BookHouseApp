13d97c3b2b0a262863ab7f3bcc812db3879990dc
rsrizwan77 <rsrizwan77@gmail.com>
2020-11-12 11:35:01 +0530

    Updated controller and services - Recommendation

--- recommendation-service/src/main/java/com/stackroute/recommendationservice/service/RecommendationBookService.java
+++ recommendation-service/src/main/java/com/stackroute/recommendationservice/service/RecommendationBookService.java
@@ -1,20 +1,19 @@
 package com.stackroute.recommendationservice.service;
+
 import java.util.List;
 
 import com.stackroute.recommendationservice.exception.RecommendationBookAlreadyExistsException;
 import com.stackroute.recommendationservice.exception.RecommendationBookNotFoundException;
 import com.stackroute.recommendationservice.model.RecommendationBook;
 
-
-
 public interface RecommendationBookService {
-	
-	boolean saveRecommendationBook(RecommendationBook recommendationbook) throws RecommendationBookAlreadyExistsException;
 
 	boolean deleteRecommendationBookById(int id) throws RecommendationBookNotFoundException;
 
 	RecommendationBook getRecommendationBookById(int id) throws RecommendationBookNotFoundException;
 
 	List<RecommendationBook> getMyRecommendationBooks(String userId);
+
+	boolean saveFavouriteBook(RecommendationBook recommendationbook) throws RecommendationBookAlreadyExistsException;
 
 }
--- recommendation-service/src/main/java/com/stackroute/recommendationservice/service/RecommendationBookServiceImpl.java
+++ recommendation-service/src/main/java/com/stackroute/recommendationservice/service/RecommendationBookServiceImpl.java
@@ -1,26 +1,30 @@
 package com.stackroute.recommendationservice.service;
 
+
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.stackroute.recommendationservice.exception.RecommendationBookAlreadyExistsException;
 import com.stackroute.recommendationservice.exception.RecommendationBookNotFoundException;
 import com.stackroute.recommendationservice.model.RecommendationBook;
 import com.stackroute.recommendationservice.repository.RecommendationBookRepository;
 
-public class RecommendationBookServiceImplementation implements RecommendationBookService {
 
+
+@Service
+public class RecommendationBookServiceImpl implements RecommendationBookService {
+	
 	@Autowired
 	private RecommendationBookRepository recommendationbookrepository;
 
 	@Override
-	public boolean saveRecommendationBook(RecommendationBook recommendationbook)
-			throws RecommendationBookAlreadyExistsException {
-		Optional<RecommendationBook> obj = recommendationbookrepository.findById(recommendationbook.getId());
-		if (obj.isPresent()) {
-			throw new RecommendationBookAlreadyExistsException("Book already exists, cannot be saved");
+	public boolean saveFavouriteBook(RecommendationBook recommendationbook) throws RecommendationBookAlreadyExistsException {
+		Optional<RecommendationBook> obj=recommendationbookrepository.findById(recommendationbook.getId());
+		if(obj.isPresent()){
+		throw new RecommendationBookAlreadyExistsException("Book already exists, cannot be saved");
 		}
 		recommendationbookrepository.save(recommendationbook);
 		return true;
@@ -29,7 +33,7 @@ public class RecommendationBookServiceImplementation implements RecommendationBo
 	@Override
 	public boolean deleteRecommendationBookById(int id) throws RecommendationBookNotFoundException {
 		RecommendationBook obj = recommendationbookrepository.findById(id).orElse(null);
-		if (obj == null) {
+		if (obj==null){
 			throw new RecommendationBookNotFoundException("Book not found, cannot be deleted.");
 		}
 		recommendationbookrepository.delete(obj);
@@ -38,8 +42,8 @@ public class RecommendationBookServiceImplementation implements RecommendationBo
 
 	@Override
 	public RecommendationBook getRecommendationBookById(int id) throws RecommendationBookNotFoundException {
-		RecommendationBook obj = recommendationbookrepository.findById(id).get();
-		if (obj == null) {
+		RecommendationBook obj=recommendationbookrepository.findById(id).get();
+		if(obj==null){
 			throw new RecommendationBookNotFoundException("Book not found");
 		}
 		return obj;
@@ -49,4 +53,5 @@ public class RecommendationBookServiceImplementation implements RecommendationBo
 	public List<RecommendationBook> getMyRecommendationBooks(String userId) {
 		return recommendationbookrepository.findByUserId(userId);
 	}
+
 }
