e0e90d87b74e430049ac74facda6c1242aacfa29
rsrizwan77 <rsrizwan77@gmail.com>
2020-11-13 10:57:03 +0530

    Updated recommendation service

--- recommendation-service/src/main/java/com/stackroute/recommendationservice/repository/RecommendationBookRepository.java
+++ recommendation-service/src/main/java/com/stackroute/recommendationservice/repository/RecommendationBookRepository.java
@@ -10,5 +10,7 @@ import com.stackroute.recommendationservice.model.RecommendationBook;
 public interface RecommendationBookRepository extends MongoRepository<RecommendationBook,Integer> {
 	
 	List<RecommendationBook> findByUserId(String userId);
+	List<RecommendationBook> findAll();
+	
 }
 
