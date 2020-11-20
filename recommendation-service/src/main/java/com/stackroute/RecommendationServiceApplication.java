157e6b61f2b0a1a2efb34b12a3fd0cf1c15328f9
rsrizwan77 <rsrizwan77@gmail.com>
2020-11-20 15:25:50 +0530

    Rectified issue with recommendation service

--- recommendation-service/src/main/java/com/stackroute/RecommendationServiceApplication.java
+++ recommendation-service/src/main/java/com/stackroute/RecommendationServiceApplication.java
@@ -2,8 +2,8 @@ package com.stackroute;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-@EnableEurekaClient
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
 @SpringBootApplication
 public class RecommendationServiceApplication {
 
--- recommendation-service/src/main/java/com/stackroute/model/RecommendationBook.java
+++ recommendation-service/src/main/java/com/stackroute/model/RecommendationBook.java
@@ -1,8 +1,7 @@
 package com.stackroute.model;
 
+import javax.persistence.*;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection ="recommendation")
